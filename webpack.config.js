10a2f84842866212d361d55ea6eb31abaedf61e6
robertogl23 <dev.robertgl@gmail.com>
2020-06-01 15:17:57 -0500

    config scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	watch: true,
 	devtool: "source-map",
 	mode: "production",
 	entry: "./src/index.js",
