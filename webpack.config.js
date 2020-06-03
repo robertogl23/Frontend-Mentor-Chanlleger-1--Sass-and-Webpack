082351085d3d7e211e53e3d2bb1ba71994e1a720
robertogl23 <dev.robertgl@gmail.com>
2020-06-02 19:28:15 -0500

    configurando deploy

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
 	entry: "./src/index.js",
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"),
+		publicPath: "/Frontend-Mentor-Chanlleger-1--Sass-and-Webpack/"
+		
 	},
 	module: {
 		rules: [
